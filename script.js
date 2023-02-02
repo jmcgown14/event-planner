71643782ffc62ef68e4a79d0bbaafbd7f3292d8c
Johnny McGown <jmac3292@gmail.com>
2023-02-01 18:09:31 -0700

    Worked with tutor to clean up local storage

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
-// TODO: Add code to display the current date in the header of the page.
-
-
 $(document).ready(function() {
   // Save Button Click
   $(".saveBtn").on('click', function () {
     // next are steps of saving the individual text in the boxes and how to associate that correctly in local storage to the correct box
-    var info = document.getElementsByClassName('description');
-    var timeID = document.getElementById('');
+
+    var info = $(this).siblings(".description").val()
+    console.log(info);
+    var timeID = $(this).parent().attr("id")
+    console.log(timeID);
+
+    localStorage.setItem(timeID, info)
   });
 
-  //what to use with .getitem to pull from local stogare?
+  //what to use with .getitem to pull from local stogare
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
 
   var currentDate = (dayjs().format('ddd, MMMM D'));
   document.getElementById("currentDate").innerHTML = currentDate;
@@ -58,26 +50,3 @@ $(document).ready(function() {
     }
   });
 });
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-  // Eventlistener on the save button
-    // create a variable, get the value
-    // set it to local storage (what will data in local storage look like?)
-      // times: [{},{}]
-
-
-
-
-
-// WHEN I refresh the page
-// THEN the saved events persist (confirms that data was entered into local storage)
-  // getting from localStorage
-  // Parse the data from localStorage to turn it back into an object (if the data was stored as a JSON)
-
-// Look at miniproject = should match very well
-// Activity 10 - Event delegation
-// Activity 2 - jquerey methods refresher addClass
-// activities for localStorage
-// Activity 8 potentially for DOM traversal in regards to save buttons
